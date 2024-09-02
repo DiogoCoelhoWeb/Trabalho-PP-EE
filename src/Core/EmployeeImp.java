e99c3d1a669682f5c0ce11a5698161be7c2b60bc
Diogo Coelho <diogoamcoelho16@gmail.com>
2024-09-02 17:00:11 +0100

    Workers and Materials Management

---
+++ src/Core/EmployeeImp.java
@@ -0,0 +1,10 @@
+/*
+ * Nome: Diogo António Martins Coelho
+ * Número: 8230387
+ * Turma: LEIT4
+ */
+
+package Core;
+
+public class EmployeeImp {
+}
---
+++ src/ESTGConstroi/EmployeeImp.java
@@ -0,0 +1,8 @@
+/*
+* Nome: Diogo António Martins Coelho
+* Número: 8230387
+* Turma: LEIT4
+*/
+
+package ESTGConstroi;public class EmployeeImp {
+}
